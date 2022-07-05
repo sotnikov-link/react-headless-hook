e256172cf72e49ec607073e90a65abffdab3cd21
Valeriy Sotnikov <sotnikov.link@mail.ru>
2022-07-05 22:09:40 +0300

    yarn init --workspace

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { a } from 'use-headless'
 
 const container = document.getElementById('app')
-const root = createRoot(container)
 
-root.render(<>{a}</>)
+if (container) {
+  const root = createRoot(container)
+  root.render(<>{a}</>)
+} else {
+  throw Error('container is null')
+}
